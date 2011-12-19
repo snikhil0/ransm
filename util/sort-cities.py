f96f180a40cd50144391b0bc3a7b297cd68d237d
Martijn van Exel <mvexel@gmail.com>
2011-12-19 17:32:44 -0600

    added file check to sort cities

--- util/sort-cities.py
+++ util/sort-cities.py
@@ -1,11 +1,15 @@
 from subprocess import call
 import re
+import os
 import glob
 
 osmosisbinary = '/osm/software/osmosis-latest/bin/osmosis'
-filesdir = '/osm/planet/zillow/*.pbf'
+filesdir = '/osm/planet/census-combinedstatisticalareas-2010/*.pbf'
 
 for infile in glob.glob(filesdir):
+    if 'sorted' in infile:
+        continue
     outfile = re.sub('.osm.pbf', '-sorted.osm.pbf', infile)
-    print outfile
+    if os.path.exists(outfile):
+        continue
     call([osmosisbinary, '--rb', infile, '--sort', '--wb', outfile])
